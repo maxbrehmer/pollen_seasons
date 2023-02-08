3ff51bd030d3125e9f512572033961381ed31fb8
Max Brehmer <max.brehmer01@gmail.com>
2023-02-08 16:52:34 +0100

    Restructiuring of references. Started discussing materials used.

--- data_wrangling.R
+++ data_wrangling.R
@@ -1,5 +1,5 @@
 data <- read_csv("data/pollen_counts.csv")
 
-df <- data %>% arrange(station, lat_name, date)
+df <- data %>% arrange(station, lat_name, date) %>% mutate(factor_count = count*factor)
 
 print(unique(df$lat_name))
