77afca68f41c8d982154dc37b52f223bfe56e95d
Max Brehmer <max.brehmer01@gmail.com>
2023-02-14 20:46:26 +0100

    deleted problematic .tex file

--- analysis.R
+++ analysis.R
@@ -8,13 +8,6 @@ pic1990 <- df %>% filter(year == 1990) %>% ggplot(aes(x = date, y = lat_name)) +
   theme_ridges() +
   facet_wrap(~ station)
 
-#ggplot(lincoln_weather, aes(x = `Mean Temperature [F]`, y = `Month`, fill = ..x..)) +
-#  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
-#  scale_fill_viridis(name = "Temp. [F]", option = "C") +
-#  labs(title = 'Temperatures in Lincoln NE in 2016') +
-#  theme_ipsum() +
-#  theme(
-#    legend.position="none",
-#    panel.spacing = unit(0.1, "lines"),
-#    strip.text.x = element_text(size = 8)
-#  )
+data_structure <- data.frame("Variable" = c("Station", "Pollen type", "Date", "Factor"), "Type" = c("categorical", "categorical", "continous", "continous"), "Decription" = c(
+  "The geographic location of the pollen monitoring station", "", "", ""
+))
