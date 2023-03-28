531721a452eaaebb3a400e10786f801b0ade5712
Max Brehmer <max.brehmer01@gmail.com>
2023-03-28 19:27:10 +0200

    Present tables and calculated which models are statistically significant

--- models.R
+++ models.R
@@ -1,5 +1,5 @@
 # Model: Quantile regression
-qr_10 <- df %>% nest_by(lat_name, station) %>% mutate(model = list(rq(formula = greg_day ~ year, data = data, tau = 0.1))) %>% dplyr::select(c(-data))
+# qr_10 <- df %>% nest_by(lat_name, station) %>% mutate(model = list(rq(formula = greg_day ~ year, data = data, tau = 0.1))) %>% dplyr::select(c(-data))
 
 # Model: Linear regression on quantiles
 data_q10 <- df %>%
@@ -10,3 +10,10 @@ eq_10 <- data_q10 %>% ungroup() %>% nest_by(lat_name, station) %>% mutate(model 
   dplyr::select(c(-data)) %>%
   mutate(coeff = summary(model)$coefficients["year", "Estimate"], "P value" = summary(model)$coefficients["year", "Pr(>|t|)"], "Adj. R-squared" = summary(model)$adj.r.squared)
 
+data_q50 <- df %>%
+  group_by(year, lat_name, station) %>%
+  summarise(q50 = quantile(greg_day, prob = .5))
+
+eq_50 <- data_q50 %>% ungroup() %>% nest_by(lat_name, station) %>% mutate(model = list(lm(formula = q50 ~ year, data = data))) %>%
+  dplyr::select(c(-data)) %>%
+  mutate(coeff = summary(model)$coefficients["year", "Estimate"], "P value" = summary(model)$coefficients["year", "Pr(>|t|)"], "Adj. R-squared" = summary(model)$adj.r.squared)
