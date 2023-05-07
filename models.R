18ed3cb63e2b7388e00f7d39831075de27a304d8
Max Brehmer <max.brehmer01@gmail.com>
2023-05-07 04:01:05 +0200

    Added text about latitude coefficient plots

--- models.R
+++ models.R
@@ -21,7 +21,8 @@ eq_1 <- data_q1 %>% ungroup() %>% nest_by(lat_name, station, latitude) %>%
   mutate(coeff = summary(model)$coefficients["Year", "Estimate"], 
          "P value" = summary(model)$coefficients["Year", "Pr(>|t|)"], "Adj. R-squared" = summary(model)$adj.r.squared) %>%
   rowwise() %>%
-  mutate(`Predicted 2023 1% (EQ)` = as.integer(predict(lm(q1 ~ Year, data = data), newdata = data.frame(Year = 50))), 
+  mutate(`Predicted 2050 1% (EQ)` = as.integer(predict(lm(q1 ~ Year, data = data), newdata = data.frame(Year = 77))),
+         `Predicted 2023 1% (EQ)` = as.integer(predict(lm(q1 ~ Year, data = data), newdata = data.frame(Year = 50))), 
          `Predicted 1973 1% (EQ)` = as.integer(predict(lm(q1 ~ Year, data = data), newdata = data.frame(Year = 0)))) %>%
   dplyr::select(c(-data))
 
@@ -37,7 +38,8 @@ eq_50 <- data_q50 %>% ungroup() %>% nest_by(lat_name, station, latitude) %>%
   mutate(coeff = summary(model)$coefficients["Year", "Estimate"], 
          "P value" = summary(model)$coefficients["Year", "Pr(>|t|)"], "Adj. R-squared" = summary(model)$adj.r.squared) %>%
   rowwise() %>%
-  mutate(`Predicted 2023 50% (EQ)` = as.integer(predict(lm(q50 ~ Year, data = data), newdata = data.frame(Year = 50))), 
+  mutate(`Predicted 2050 50% (EQ)` = as.integer(predict(lm(q50 ~ Year, data = data), newdata = data.frame(Year = 77))),
+         `Predicted 2023 50% (EQ)` = as.integer(predict(lm(q50 ~ Year, data = data), newdata = data.frame(Year = 50))), 
          `Predicted 1973 50% (EQ)` = as.integer(predict(lm(q50 ~ Year, data = data), newdata = data.frame(Year = 0)))) %>%
   dplyr::select(c(-data))
 
@@ -53,18 +55,21 @@ eq_95 <- data_q95 %>% ungroup() %>% nest_by(lat_name, station, latitude) %>%
   mutate(coeff = summary(model)$coefficients["Year", "Estimate"], 
          "P value" = summary(model)$coefficients["Year", "Pr(>|t|)"], "Adj. R-squared" = summary(model)$adj.r.squared) %>%
   rowwise() %>%
-  mutate(`Predicted 2023 95% (EQ)` = as.integer(predict(lm(q95 ~ Year, data = data), newdata = data.frame(Year = 50))), 
+  mutate(`Predicted 2050 95% (EQ)` = as.integer(predict(lm(q95 ~ Year, data = data), newdata = data.frame(Year = 77))),
+         `Predicted 2023 95% (EQ)` = as.integer(predict(lm(q95 ~ Year, data = data), newdata = data.frame(Year = 50))), 
          `Predicted 1973 95% (EQ)` = as.integer(predict(lm(q95 ~ Year, data = data), newdata = data.frame(Year = 0)))) %>%
   dplyr::select(c(-data))
 
 
 # Joining together the relevant information from each table
 eq_joined <- eq_1 %>%
-  dplyr::select(lat_name, station, latitude, coeff, `P value`, `Predicted 2023 1% (EQ)`, `Predicted 1973 1% (EQ)`) %>%
+  dplyr::select(lat_name, station, latitude, coeff, `P value`, `Predicted 1973 1% (EQ)`, `Predicted 2023 1% (EQ)`, `Predicted 2050 1% (EQ)`) %>%
   rename("coefficient 1%" = coeff, "P value 1%" = `P value`) %>%
-  left_join(eq_50 %>% rename("coefficient 50%" = coeff, "P value 50%" = `P value`) %>% dplyr::select(lat_name, station, latitude, "coefficient 50%", "P value 50%", `Predicted 2023 50% (EQ)`, `Predicted 1973 50% (EQ)`),
+  left_join(eq_50 %>% rename("coefficient 50%" = coeff, "P value 50%" = `P value`) %>% 
+              dplyr::select(lat_name, station, latitude, "coefficient 50%", "P value 50%", `Predicted 1973 50% (EQ)`, `Predicted 2023 50% (EQ)`, `Predicted 2050 50% (EQ)`),
             by = c("lat_name", "station", "latitude")) %>%
-  left_join(eq_95 %>% rename("coefficient 95%" = coeff, "P value 95%" = `P value`) %>% dplyr::select(lat_name, station, latitude, "coefficient 95%", "P value 95%", `Predicted 2023 95% (EQ)`, `Predicted 1973 95% (EQ)`),
+  left_join(eq_95 %>% rename("coefficient 95%" = coeff, "P value 95%" = `P value`) %>% 
+              dplyr::select(lat_name, station, latitude, "coefficient 95%", "P value 95%", `Predicted 1973 95% (EQ)`, `Predicted 2023 95% (EQ)`, `Predicted 2050 95% (EQ)`),
             by = c("lat_name", "station", "latitude")) %>%
   mutate(across(where(is.numeric), ~ round(., 3)))
 
@@ -73,19 +78,19 @@ eq_joined <- eq_1 %>%
 
 # Tables for visualizing the EQ models
 joined_eq_1 <- eq_joined %>% arrange(`coefficient 1%`) %>% 
-  dplyr::select(lat_name, station, latitude, `coefficient 1%`, `P value 1%`, `Predicted 2023 1% (EQ)`, `Predicted 1973 1% (EQ)`) %>%
+  dplyr::select(lat_name, station, latitude, `coefficient 1%`, `P value 1%`, `Predicted 1973 1% (EQ)`, `Predicted 2023 1% (EQ)`, `Predicted 2050 1% (EQ)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 1%", 
-         "P value (EQ)" = "P value 1%", "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)", "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)")
+         "P value (EQ)" = "P value 1%", "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)", "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)", "Predicted 2050 (EQ)" = "Predicted 2050 1% (EQ)")
 
 joined_eq_50 <- eq_joined %>% arrange(`coefficient 50%`) %>% 
-  dplyr::select(lat_name, station, latitude, `coefficient 50%`, `P value 50%`, `Predicted 2023 50% (EQ)`, `Predicted 1973 50% (EQ)`) %>%
+  dplyr::select(lat_name, station, latitude, `coefficient 50%`, `P value 50%`, `Predicted 1973 50% (EQ)`, `Predicted 2023 50% (EQ)`, `Predicted 2050 50% (EQ)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 50%", 
-         "P value (EQ)" = "P value 50%", "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)", "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)")
+         "P value (EQ)" = "P value 50%", "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)", "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)", "Predicted 2050 (EQ)" = "Predicted 2050 50% (EQ)")
 
 joined_eq_95 <- eq_joined %>% arrange(`coefficient 95%`) %>% 
-  dplyr::select(lat_name, station, latitude, `coefficient 95%`, `P value 95%`, `Predicted 2023 95% (EQ)`, `Predicted 1973 95% (EQ)`) %>%
+  dplyr::select(lat_name, station, latitude, `coefficient 95%`, `P value 95%`, `Predicted 1973 95% (EQ)`, `Predicted 2023 95% (EQ)`, `Predicted 2050 95% (EQ)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 95%", 
-         "P value (EQ)" = "P value 95%", "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)", "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)")
+         "P value (EQ)" = "P value 95%", "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)", "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)", "Predicted 2050 (EQ)" = "Predicted 2050 95% (EQ)")
 
 
 
--- qr_models.R
+++ qr_models.R
@@ -1,7 +1,9 @@
 # Model: Quantile regression
 qr_1 <- df %>% nest_by(station, lat_name) %>% mutate(model = list(rq(formula = greg_day ~ Year, data = data, tau = 0.01))) %>% 
   rowwise() %>%
-  mutate(`Predicted 2023 1% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+  mutate(`Predicted 2050 1% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+                                                          data = data, tau = 0.01), newdata = data.frame(Year = 77))),
+         `Predicted 2023 1% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                           data = data, tau = 0.01), newdata = data.frame(Year = 50))), 
          `Predicted 1973 1% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                           data = data, tau = 0.01), newdata = data.frame(Year = 0)))) %>%
@@ -10,7 +12,9 @@ qr_1 <- df %>% nest_by(station, lat_name) %>% mutate(model = list(rq(formula = g
 
 qr_50 <- df %>% nest_by(lat_name, station) %>% mutate(model = list(rq(formula = greg_day ~ Year, data = data, tau = 0.5))) %>% 
   rowwise() %>%
-  mutate(`Predicted 2023 50% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+  mutate(`Predicted 2050 50% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+                                                          data = data, tau = 0.5), newdata = data.frame(Year = 77))),
+         `Predicted 2023 50% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                            data = data, tau = 0.5), newdata = data.frame(Year = 50))), 
          `Predicted 1973 50% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                            data = data, tau = 0.5), newdata = data.frame(Year = 0)))) %>%
@@ -19,7 +23,9 @@ qr_50 <- df %>% nest_by(lat_name, station) %>% mutate(model = list(rq(formula = 
 
 qr_95 <- df %>% nest_by(lat_name, station) %>% mutate(model = list(rq(formula = greg_day ~ Year, data = data, tau = 0.95))) %>%
   rowwise() %>%
-  mutate(`Predicted 2023 95% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+  mutate(`Predicted 2050 95% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
+                                                          data = data, tau = 0.95), newdata = data.frame(Year = 77))),
+         `Predicted 2023 95% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                            data = data, tau = 0.95), newdata = data.frame(Year = 50))), 
          `Predicted 1973 95% (QR)` = as.integer(predict(rq(greg_day ~ Year, 
                                                            data = data, tau = 0.95), newdata = data.frame(Year = 0)))) %>%
@@ -40,46 +46,58 @@ joined <- eq_joined %>% left_join(qr_1 %>% dplyr::select(c(-model)), by = c("lat
 # Tables for visualizing the EQ models
 joined_eq_1 <- joined %>% arrange(`coefficient 1%`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 1%`, `P value 1%`, `coefficient 1% (QR)`, 
-                `Predicted 2023 1% (EQ)`, `Predicted 1973 1% (EQ)`, `Predicted 2023 1% (QR)`, `Predicted 1973 1% (QR)`) %>%
+                `Predicted 1973 1% (EQ)`, `Predicted 2023 1% (EQ)`, `Predicted 2050 1% (EQ)`, `Predicted 1973 1% (QR)`, `Predicted 2023 1% (QR)`, `Predicted 2050 1% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 1%", 
-         "P value (EQ)" = "P value 1%", "Coefficient (QR)" = "coefficient 1% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 1% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 1% (QR)")
+         "P value (EQ)" = "P value 1%", "Coefficient (QR)" = "coefficient 1% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 1% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 1% (QR)",
+         "Predicted 2050 (EQ)" = "Predicted 2050 1% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 1% (QR)")
 
 joined_eq_50 <- joined %>% arrange(`coefficient 50%`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 50%`, `P value 50%`, `coefficient 50% (QR)`, 
-                `Predicted 2023 50% (EQ)`, `Predicted 1973 50% (EQ)`, `Predicted 2023 50% (QR)`, `Predicted 1973 50% (QR)`) %>%
+                `Predicted 1973 50% (EQ)`, `Predicted 2023 50% (EQ)`, `Predicted 2050 50% (EQ)`, `Predicted 1973 50% (QR)`, `Predicted 2023 50% (QR)`, `Predicted 2050 50% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 50%", 
-         "P value (EQ)" = "P value 50%", "Coefficient (QR)" = "coefficient 50% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 50% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 50% (QR)")
+         "P value (EQ)" = "P value 50%", "Coefficient (QR)" = "coefficient 50% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 50% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 50% (QR)",
+         "Predicted 2050 (EQ)" = "Predicted 2050 50% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 50% (QR)")
 
 joined_eq_95 <- joined %>% arrange(`coefficient 95%`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 95%`, `P value 95%`, `coefficient 95% (QR)`, 
-                `Predicted 2023 95% (EQ)`, `Predicted 1973 95% (EQ)`, `Predicted 2023 95% (QR)`, `Predicted 1973 95% (QR)`) %>%
+                `Predicted 1973 95% (EQ)`, `Predicted 2023 95% (EQ)`, `Predicted 2050 95% (EQ)`, `Predicted 1973 95% (QR)`, `Predicted 2023 95% (QR)`, `Predicted 2050 95% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 95%", 
-         "P value (EQ)" = "P value 95%", "Coefficient (QR)" = "coefficient 95% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 95% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 95% (QR)")
+         "P value (EQ)" = "P value 95%", "Coefficient (QR)" = "coefficient 95% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 95% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 95% (QR)",
+         "Predicted 2050 (EQ)" = "Predicted 2050 95% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 95% (QR)")
 
 # Tables for visualizing the QR models
 joined_qr_1 <- joined %>% arrange(`coefficient 1% (QR)`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 1%`, `P value 1%`, `coefficient 1% (QR)`, 
-                `Predicted 2023 1% (EQ)`, `Predicted 2023 1% (QR)`, `Predicted 1973 1% (EQ)`, `Predicted 1973 1% (QR)`) %>%
+                `Predicted 1973 1% (EQ)`, `Predicted 1973 1% (QR)`, `Predicted 2023 1% (EQ)`, `Predicted 2023 1% (QR)`, `Predicted 2050 1% (EQ)`, `Predicted 2050 1% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 1%", 
-         "P value (EQ)" = "P value 1%", "Coefficient (QR)" = "coefficient 1% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 1% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 1% (QR)")
+         "P value (EQ)" = "P value 1%", "Coefficient (QR)" = "coefficient 1% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 1% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 1% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 1% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 1% (QR)", 
+         "Predicted 2050 (EQ)" = "Predicted 2050 1% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 1% (QR)")
 
 joined_qr_50 <- joined %>% arrange(`coefficient 50% (QR)`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 50%`, `P value 50%`, `coefficient 50% (QR)`, 
-                `Predicted 2023 50% (EQ)`, `Predicted 2023 50% (QR)`, `Predicted 1973 50% (EQ)`, `Predicted 1973 50% (QR)`) %>%
+                `Predicted 1973 50% (EQ)`, `Predicted 1973 50% (QR)`, `Predicted 2023 50% (EQ)`, `Predicted 2023 50% (QR)`, `Predicted 2050 50% (EQ)`, `Predicted 2050 50% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 50%", 
-         "P value (EQ)" = "P value 50%", "Coefficient (QR)" = "coefficient 50% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 50% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 50% (QR)")
+         "P value (EQ)" = "P value 50%", "Coefficient (QR)" = "coefficient 50% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 50% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 50% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 50% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 50% (QR)", 
+         "Predicted 2050 (EQ)" = "Predicted 2050 50% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 50% (QR)")
 
 joined_qr_95 <- joined %>% arrange(`coefficient 95% (QR)`) %>% 
   dplyr::select(lat_name, station, latitude, `coefficient 95%`, `P value 95%`, `coefficient 95% (QR)`, 
-                `Predicted 2023 95% (EQ)`, `Predicted 2023 95% (QR)`, `Predicted 1973 95% (EQ)`, `Predicted 1973 95% (QR)`) %>%
+                `Predicted 1973 50% (EQ)`, `Predicted 1973 50% (QR)`, `Predicted 2023 50% (EQ)`, `Predicted 2023 50% (QR)`, `Predicted 2050 95% (EQ)`, `Predicted 2050 95% (QR)`) %>%
   rename("Species" = lat_name, "Location" = station, "Latitude" = latitude, "Coefficient (EQ)" = "coefficient 95%", 
-         "P value (EQ)" = "P value 95%", "Coefficient (QR)" = "coefficient 95% (QR)", "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)",
-         "Predicted 2023 (QR)" = "Predicted 2023 95% (QR)", "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 95% (QR)")
+         "P value (EQ)" = "P value 95%", "Coefficient (QR)" = "coefficient 95% (QR)", 
+         "Predicted 1973 (EQ)" = "Predicted 1973 95% (EQ)", "Predicted 1973 (QR)" = "Predicted 1973 95% (QR)", 
+         "Predicted 2023 (EQ)" = "Predicted 2023 95% (EQ)", "Predicted 2023 (QR)" = "Predicted 2023 95% (QR)", 
+         "Predicted 2050 (EQ)" = "Predicted 2050 95% (EQ)", "Predicted 2050 (QR)" = "Predicted 2050 95% (QR)")
 
 
 
