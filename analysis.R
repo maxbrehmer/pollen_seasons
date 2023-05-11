c045c7b8568e7b2d787628e665ec9950483ee8a2
Max Brehmer <max.brehmer01@gmail.com>
2023-05-11 15:57:02 +0200

    Fixed final appendix table

--- analysis.R
+++ analysis.R
@@ -9,10 +9,12 @@ ridges <- df %>% filter(year %in% c(1973, 1974, 1975, 1976, 1977, 2018, 2019, 20
   theme_ridges()
 
 
-data_structure <- data.frame("Variable" = c("Station", "Species", "Date", "Count", "Factor", "Latitude"), "Type" = c("categorical", "categorical", "continous", "continous", "continous", "continous"), "Decription" = c(
-  "Geographic location of the pollen monitoring station.", "Genus of the recorded pollen counts.", "Gregorian calendar date on which the airborne pollen were registered.", 
-  "Number of individual pollen counted.", "Reference variable for the size of the microscope used.", "Northern latitudinal cooardinates of said station."
-))
+data_structure <- data.frame("Variable" = c("Station", "Species", "Date", "Count", "Factor", "Latitude"), 
+                             "Type" = c("Categorical", "Categorical", "Continous", "Continous", "Continous", "Continous"), 
+                             "Decription" = c("Geographic location of the pollen monitoring station.", 
+                                              "Genus of the recorded pollen counts.", "Gregorian calendar date on which the airborne pollen were registered.", 
+                                              "Number of individual pollen counted.", "Reference variable for the size of the microscope used.", 
+                                              "Northern latitudinal cooardinates of said station."))
 
 stations <- data.frame("Station" = c("Umeå", "Eskilstuna", "Stockholm", "Norrköping", "Jönköping", "Västervik", "Göteborg", "Malmö"), "Latitude" = c(62.83, 59.37, 59.33, 58.59, 57.78, 57.76, 57.71, 55.60), 
                        "Pollen_types" = c("Alnus, Betula, Poaceae, Ulmus (1979), Salix (1981), Corylus (1987), Quercus (1995)", 
@@ -24,7 +26,7 @@ stations <- data.frame("Station" = c("Umeå", "Eskilstuna", "Stockholm", "Norrk�
                                           "Alnus, Betula, Corylus, Poaceae, Quercus, Salix, Ulmus (1979)",
                                           "Alnus, Betula, Corylus, Poaceae, Quercus, Salix, Ulmus (1979)"))
 
-colnames(stations) <- c("Station", "Latitude", "Pollen genus (since ...)")
+colnames(stations) <- c("Station", "Latitude", "Pollen genus (since the year)")
 
 eskil <- df %>% filter(station == "Eskilstuna") %>% arrange(lat_name, date)
 sthlm <- df %>% filter(station == "Stockholm") %>% arrange(lat_name, date)
